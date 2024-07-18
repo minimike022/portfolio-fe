7fcafaee38c9c0a1df1b1b958f3452de62f546ce
MiniMike <minimike156@gmail.com>
2024-07-18 13:53:52 +0800

    Removing a links

--- src/layouts/navigation/Navigation.vue
+++ src/layouts/navigation/Navigation.vue
@@ -5,10 +5,7 @@
     <div class="flex items-center justify-between px-8 py-4">
         <h1 class="text-primary">Solo Dev</h1>
         <div>
-            <a class="ml-8" href="#home">Home</a>
-            <a class="ml-8" href="#about">About</a>
-            <a class="ml-8" href="#projects">Projects</a>
-            <a class="ml-8" href="#contact">Contact</a>
+            
         </div>
     </div>
 </template>
