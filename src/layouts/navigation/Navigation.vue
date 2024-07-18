7b679c6def81e2d8684aef6f1f08d44755d9ec7e
MiniMike <minimike156@gmail.com>
2024-07-18 13:55:00 +0800

    Test 1

--- src/layouts/navigation/Navigation.vue
+++ src/layouts/navigation/Navigation.vue
@@ -5,7 +5,7 @@
     <div class="flex items-center justify-between px-8 py-4">
         <h1 class="text-primary">Solo Dev</h1>
         <div>
-            
+            routes
         </div>
     </div>
 </template>
