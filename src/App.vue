af1e019e4b7f447ba7db74b8eae12fcb2c226d7c
MiniMike <minimike156@gmail.com>
2024-07-18 13:33:21 +0800

    Git Initialized

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import About from './views/About.vue';
+import Home from './views/Home.vue';
+import Projects from './views/Projects.vue';
+import Contact from './views/Contact.vue';
+</script>
+
+<template>
+  <Home/>
+  <Projects/>
+  <About/>
+  <Contact/>
+</template>
