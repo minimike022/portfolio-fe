b132708cf9f235926f95fbf85446a9b6910c8ca4
MiniMike <minimike156@gmail.com>
2024-07-18 13:51:39 +0800

    Adding tailwindcsss and layouts

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,19 @@ import About from './views/About.vue';
 import Home from './views/Home.vue';
 import Projects from './views/Projects.vue';
 import Contact from './views/Contact.vue';
+import Navigation from './layouts/navigation/Navigation.vue';
 </script>
 
+
 <template>
-  <Home/>
-  <Projects/>
-  <About/>
-  <Contact/>
+  <div>
+    <Navigation/>
+    <div>
+      <Home id="home"/>
+      <Projects id="project"/>
+      <About id="about"/>
+      <Contact id="contact"/>
+    </div>
+  </div>
+
 </template>
