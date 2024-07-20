192f33b3969cf2878eb67cfb15e753ef680d7b00
MiniMike <minimike156@gmail.com>
2024-07-20 13:25:37 +0800

    Adding forms

--- src/components/ContactForms.vue
+++ src/components/ContactForms.vue
@@ -1,6 +1,16 @@
 <script setup>
+import {ref} from 'vue'
+
+const response_forms = ref({
+    full_name: '',
+    email: '',
+    subject: '',
+    details: ''
+})
+
+
 const submit_form = () => {
-    console.log("Form_Submitted!")
+    console.log(response_forms.value)
 }
 </script>
 
@@ -8,11 +18,11 @@ const submit_form = () => {
     <div class="w-[50dvh] grid grid-cols-1 gap-y-2">
         <h1 class="text-white text-lg">Send me a message🚀</h1>
         <form @submit.prevent="submit_form()" class="grid grid-cols-1 gap-y-4">
-            <input type="text" placeholder="Full Name" class="h-[7dvh] placeholder-gray-600 bg-[#0c1827]  rounded-md pl-4 py-[.7dvh] text-white outline-none focus:border focus:border-[#193352]" required>
-            <input type="email" placeholder="Email" class="h-[7dvh] placeholder-gray-600 bg-[#0c1827] rounded-md pl-4 py-[.7dvh] text-white outline-none focus:border focus:border-[#193352]" required>
-            <input type="text" placeholder="Subject" class="h-[7dvh] placeholder-gray-600 bg-[#0c1827] rounded-md pl-4 py-[.7dvh] text-white outline-none focus:border focus:border-[#193352]" required>
+            <input type="text" v-model="response_forms.full_name" placeholder="Full Name" class="h-[7dvh] placeholder-gray-600 bg-[#0c1827]  rounded-md pl-4 py-[.7dvh] text-white outline-none focus:border focus:border-[#193352]" required>
+            <input type="email" v-model="response_forms.email" placeholder="Email" class="h-[7dvh] placeholder-gray-600 bg-[#0c1827] rounded-md pl-4 py-[.7dvh] text-white outline-none focus:border focus:border-[#193352]" required>
+            <input type="text" v-model="response_forms.subject" placeholder="Subject" class="h-[7dvh] placeholder-gray-600 bg-[#0c1827] rounded-md pl-4 py-[.7dvh] text-white outline-none focus:border focus:border-[#193352]" required>
             <h1 class="text-white text-sm">Tell me more about the project</h1>
-            <textarea placeholder="Tell me more.." name="" id="" class="bg-[#0c1827] h-[20dvh] placeholder-gray-600 px-4 py-[.7vh] rounded-md text-white outline-none focus:border focus:border-[#193352]"></textarea>
+            <textarea v-model="response_forms.details" placeholder="Tell me more.." name="" id="" class="bg-[#0c1827] h-[20dvh] placeholder-gray-600 px-4 py-[.7vh] rounded-md text-white outline-none focus:border focus:border-[#193352]"></textarea>
             <div>
                 <input type="submit" value="Send Message" class="text-sm text-white px-4 py-2 rounded-md bg-[#76C79B]">
             </div>
