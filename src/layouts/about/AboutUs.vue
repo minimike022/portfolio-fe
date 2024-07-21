404412fc8fd998808a050fd554556fdf177e9e2a
MiniMike <minimike156@gmail.com>
2024-07-21 12:27:49 +0800

    fixing responsiveness issues

--- src/layouts/about/AboutUs.vue
+++ src/layouts/about/AboutUs.vue
@@ -3,18 +3,25 @@
 <template>
     <!-- Anchor -->
     <div id="about" class="h-10"></div>
-    <div class="mb-12 mx-10 md:mt-[11dvh] md:flex md:mx-[33dvh]">
+    <div class="mb-12 mx-10 md:mt-[11dvh] md:flex md:mx-[33dvh] relative">
         <!-- Contents -->
         <div class="w-auto md:w-[80dvh] text-white">
-            <div class="flex justify-between items-center">
-                <div class="w-[25dvh] h-[.4dvh] bg-[#8E8E8E]"></div>
-                <div class="w-auto">
-                    <h1 class="text-primary text-2xl md:text-3xl">About Me</h1>
+            <div class="flex items-center">
+                    <div class="w-full flex justify-center">
+                        <h1 class="text-primary text-xl md:text-3xl">About Me</h1>
+                    </div>
+                    <div class="w-full h-[.4dvh] bg-[#8E8E8E]"></div>
+
+            </div>
+            <div class="mt-2 mb-8 md:ml-6 w-[40dvh] h-[40dvh] relative md:absolute md:top-0 md:right-0">
+                <div class="absolute w-[40dvh] h-[40dvh] border-2 border-primary top-6 left-16 -z-10 rounded-md"></div>
+                <div class="absolute w-[40dvh] h-[40dvh] border-2 border-primary top-3 left-12 -z-10 rounded-md"></div>
+                <div class="absolute top-0 left-9 w-[40dvh] h-[40dvh]">
+                    <img src="/src/assets/profilePhoto/profile.jpg" alt="" class="rounded-md">
                 </div>
-                <div class="w-[25dvh] h-[.4dvh] bg-[#8E8E8E]"></div>
             </div>
             <div>
-                <p class=" text-gray-200 mt-2 text-sm md:text-base">
+                <p class=" text-gray-200 mt-2 text-xs text-justify md:text-base">
                     Hello! My name is Michael C. Eduardo Jr. I’m a Software Engineer based in Philippines, who really
                     loves and enjoys
                     building things.
@@ -30,7 +37,7 @@
                     </span> ensuring that each application is built to high quality standards.
                     <br><br>Here are the languages and frameworks that I’ve been working with recently.
                     <br><br>
-                <div class="grid grid-cols-3 gap-y-2 text-sm">
+                <div class="grid grid-cols-3 gap-y-3 text-sm">
                     <li>HTML</li>
                     <li>CSS</li>
                     <li>Tailwind</li>
@@ -47,12 +54,6 @@
                 </p>
             </div>
         </div>
-        <div class="my-4 md:ml-6 w-[40dvh] h-[40dvh] relative">
-            <div class="absolute w-[40dvh] h-[40dvh] border-2 border-primary top-6 left-6 -z-10 rounded-md"></div>
-            <div class="absolute w-[40dvh] h-[40dvh] border-2 border-primary top-3 left-3 -z-10 rounded-md"></div>
-            <div class="absolute top-0 left-0 w-[40dvh] h-[40dvh]">
-                <img src="/src/assets/profilePhoto/profile.jpg" alt="" class="rounded-md">
-            </div>
-        </div>
+
     </div>
 </template>
--- src/layouts/projects/Projects.vue
+++ src/layouts/projects/Projects.vue
@@ -14,23 +14,26 @@ const projects = ref([
     <div class="flex mx-10 md:mx-0 md:justify-center mt-[11dvh]">
         <div>
             <div class="flex items-center">
-            <h1 class="text-primary text-3xl">Projects</h1>
-            <div class="ml-4 w-full h-[.4dvh] bg-[#8E8E8E]"></div>
-        </div>
-        <div class="grid grid-cols-1 gap-y-5 md:grid-cols-3 md:gap-x-5 my-4">
-            <div v-for="proj in projects" class="w-full md:w-[50dvh] text-sm text-white bg-[#15273E] p-4 rounded-md relative">
-                <div class="absolute flex left-0 px-4">
-                    <a v-if="proj.github !== ''" :href="proj.github"><img src="/src/assets/socialLogo/githubLogo.svg" alt="" class="w-[24px]"></a>
-                    <a v-if="proj.itch !== ''" :href="proj.itch" class="ml-2"><img src="/src/assets/socialLogo/playLogo.svg" alt=""></a>
-                </div>
-                <h1 class="font-bold my-2 mt-10">{{ proj.title }}</h1>
-                <h1 class="text-[#8E8E8E] my-2">{{ proj.description }}</h1>
-                <h1 class="text-white my-2">{{ proj.role }}</h1>
-                <div class="flex flex-wrap text-[#8E8E8E] text-xs my-2">
-                    <h1 v-for="lang in proj.language" class="mr-6 my-[2px]">{{ lang }}</h1>
+                <h1 class="text-primary text-3xl">Projects</h1>
+                <div class="ml-4 w-full h-[.4dvh] bg-[#8E8E8E]"></div>
+            </div>
+            <div class="grid grid-cols-1 gap-y-5 md:grid-cols-3 md:gap-x-5 my-4">
+                <div v-for="proj in projects"
+                    class="w-full md:w-[50dvh] text-sm text-white bg-[#15273E] p-4 rounded-md relative">
+                    <div class="absolute flex left-0 px-4">
+                        <a v-if="proj.github !== ''" :href="proj.github"><img
+                                src="/src/assets/socialLogo/githubLogo.svg" alt="" class="w-[24px]"></a>
+                        <a v-if="proj.itch !== ''" :href="proj.itch" class="ml-2"><img
+                                src="/src/assets/socialLogo/playLogo.svg" alt=""></a>
+                    </div>
+                    <h1 class="font-bold my-2 mt-10">{{ proj.title }}</h1>
+                    <h1 class="text-[#8E8E8E] my-2">{{ proj.description }}</h1>
+                    <h1 class="text-white my-2">{{ proj.role }}</h1>
+                    <div class="flex flex-wrap text-[#8E8E8E] text-xs my-2">
+                        <h1 v-for="lang in proj.language" class="mr-6 my-[2px]">{{ lang }}</h1>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     </div>
 </template>
