691fea79ba820e75abb8d1e08cf52185ebda4eb1
MiniMike <minimike156@gmail.com>
2024-07-23 14:43:04 +0800

    Changing download button

--- src/components/buttons/Download.vue
+++ src/components/buttons/Download.vue
@@ -1,13 +1,9 @@
 <script setup>
 
-const download_cv = () => {
-    console.log("CV Downloaded!")
-}
 </script>
 
 <template>
-    <a href="/files/resume.pdf" download> download </a>
-    <div class="transition duration-300 ease-in-out w-[25dvh] text-primary border border-primary py-2 rounded-md text-center hover:bg-primary hover:text-white cursor-pointer" @click="download_cv">
+    <a href="/files/resume.pdf" class="transition duration-300 ease-in-out w-[25dvh] text-primary border border-primary py-2 rounded-md text-center hover:bg-primary hover:text-white cursor-pointer" download>
         <h1>Download CV</h1>
-    </div>
+    </a>
 </template>
